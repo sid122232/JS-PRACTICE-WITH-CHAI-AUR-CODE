6db87d0e8786da04c05270a96dbd374c2a3e70b6
sid122232 <154446013+sid122232@users.noreply.github.com>
2024-09-17 11:34:07 +0000

    DATES AND ARRAY

---
+++ dates.js
@@ -0,0 +1,41 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr2);
+
+console.log(myArr[1]);
+
+// Array methods
+
+console.log(myArr.push(6))
+// myArr.push(7)
+// myArr.pop() removes the last element of array
+
+// myArr.unshift(9) The unshift() method of Array instances adds the specified elements to the beginning of an array and returns the new length of the array.
+
+
+// myArr.shift() The shift() method of Array instances removes the first element from an array and returns that removed element. This method changes the length of the array.
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+// console.log("A ", myArr);
+
+// const myn1 = myArr.slice(1, 3)
+
+// console.log(myn1);
+// console.log("B ", myArr);
+
+
+// const myn2 = myArr.splice(1, 3)
+// console.log("C ", myArr);
+// console.log(myn2);
