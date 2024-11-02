f6a7bd41666ac8a8b243aab1187640935517bdec
sid122232 <154446013+sid122232@users.noreply.github.com>
2024-11-02 05:47:03 +0000

    literal object , new keyword

---
+++ promises.js
@@ -0,0 +1,10 @@
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true
+        if (!error) {
+            resolve({username: "hitesh", password: "123"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
